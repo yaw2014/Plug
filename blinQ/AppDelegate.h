e7d576d848c6b4022b406ef05ad0d23498787f59
haisergeant <haisergeant@gmail.com>
2014-04-30 13:47:49 +0700

    Initial Commit

---
+++ blinQ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  blinQ
+//
+//  Created by Le Thanh Hai on 4/30/14.
+//  Copyright (c) 2014 templum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ blinQ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  blinQ
+//
+//  Created by Le Thanh Hai on 4/30/14.
+//  Copyright (c) 2014 templum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
